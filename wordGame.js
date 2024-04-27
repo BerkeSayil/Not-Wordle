6087553b99243810a8ef0ca36d35ebd6d0f88b32
Berke Sayıl <berkesayil@gmail.com>
2024-04-27 23:18:59 +0300

    Finished notwordle
    well it works, checks words to validate, even has a win animation, go try it

--- wordGame.js
+++ wordGame.js
@@ -1,20 +1,23 @@
 const letters = document.querySelectorAll('.scoreboard-letter');
 const loadingDiv = document.querySelector('.info-bar');
 const ANSWER_LENGTH = 5;
-
+const ENDROW = 6;
 
 async function init(){
     
     let currentGuess = '';
     let currentRow = 0;
+    let isLoading = true;
 
     const res = await fetch("https://words.dev-apis.com/word-of-the-day");
     const resObj = await res.json();
     const word = resObj.word.toUpperCase();
     const wordParts = word.split('');
+    let done = false;
 
 
     setLoading(false);
+    isLoading = false;
 
     function addLetter(letter){
         if(currentGuess.length < ANSWER_LENGTH){
@@ -34,9 +37,28 @@ async function init(){
             return;
         }
 
-        // TODO: validate the word
+        
 
-        // TODO: do the marking of letters
+        isLoading = true;
+        setLoading(true);
+        const res = await fetch(`https://words.dev-apis.com/validate-word`, {
+            method: "POST",
+            body: JSON.stringify({
+                word: currentGuess
+            })
+
+        });
+
+        const resObj = await res.json();
+        const validWord = resObj.validWord;
+
+        isLoading = false;
+        setLoading(false);
+
+        if(!validWord){
+            markInvalidWord(); 
+            return;
+        }
 
         const guessParts = currentGuess.split('');
         const map = makeMap(wordParts);
@@ -68,11 +90,26 @@ async function init(){
             }
         }
 
-        // TODO: did they win or lose?
-
+        if(currentGuess === word){
+            document.querySelector('.brand').classList.add('winner')
+            alert('You win!');
+            done = true;
+            return;
+        }else if(currentRow === ENDROW){
+            alert('You lose! Please try again by refreshing the page.');
+            done = true;
+            return;
+        }
 
         currentRow++;
         currentGuess = '';
+
+        if(currentRow === ENDROW){
+            alert('You lose! Please try again by refreshing the page.');
+            done = true;
+            return;
+        }
+
     }
 
 
@@ -81,7 +118,30 @@ async function init(){
         letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
     }
 
+    function markInvalidWord(){
+        alert('Invalid word! Please try another word!');
+
+        for(let i = 0 ; i < ANSWER_LENGTH; i++){
+            letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
+
+            setTimeout(function(){
+            
+                letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+                
+            } , 10);
+        }
+        
+        
+
+    }
+
     document.addEventListener('keydown', function handleKeyPress(event){
+       
+        if(done || isLoading){
+            // do  nothing
+            return;
+        }
+        
         const action = event.key;
 
         console.log(action);
